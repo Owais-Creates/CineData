c5cc96cc943254f0054f9e555668e5e361523588
Owais Ali <zaidalit00@gmail.com>
2024-07-27 21:40:48 +0530

    created footer component

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,10 +1,46 @@
-import React from 'react'
-import './Footer.scss'
+import React from "react";
+import {
+    FaFacebookF,
+    FaInstagram,
+    FaTwitter,
+    FaLinkedin,
+} from "react-icons/fa";
+
+import ContentWrapper from "../contentWrapper/ContentWrapper";
+
+import "./Footer.scss";
 
 const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
-}
+    return (
+        <footer className="footer">
+            <ContentWrapper>
+                <ul className="menuItems">
+                    <li className="menuItem">Terms Of Use</li>
+                    <li className="menuItem">Privacy-Policy</li>
+                    <li className="menuItem">About</li>
+                    <li className="menuItem">Blog</li>
+                    <li className="menuItem">FAQ</li>
+                </ul>
+                <div className="infoText">
+                Welcome to CineData, your go-to destination for comprehensive movie details and insights. Whether you're a film enthusiast or a casual viewer, our app provides you with in-depth information about your favorite movies, including cast and crew details, plot summaries, release dates, ratings, and much more. Powered by the TMDB API, MovieInfoHub ensures you stay updated with the latest and greatest in the world of cinema. Dive into the world of movies with us and discover everything you need to know about your next watch!
+                </div>
+                <div className="socialIcons">
+                    <span className="icon">
+                        <FaFacebookF />
+                    </span>
+                    <span className="icon">
+                        <FaInstagram />
+                    </span>
+                    <span className="icon">
+                        <FaTwitter />
+                    </span>
+                    <span className="icon">
+                        <FaLinkedin />
+                    </span>
+                </div>
+            </ContentWrapper>
+        </footer>
+    );
+};
 
-export default Footer
+export default Footer;
