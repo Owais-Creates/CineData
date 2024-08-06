41520f8929a103114fc2f2158106b54bf1763a5e
Owais Ali <zaidalit00@gmail.com>
2024-08-06 14:07:04 +0530

    finished

--- src/pages/404/Error404.jsx
+++ src/pages/404/Error404.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import './Error404.scss'
+import React from "react";
+import "./Error404.scss";
+import ContentWrapper from "../../components/contentWrapper/ContentWrapper";
 
-const Error404 = () => {
-  return (
-    <div>Error404</div>
-  )
-}
+const PageNotFound = () => {
+    return (
+        <div className="pageNotFound">
+            <ContentWrapper>
+                <span className="bigText">404</span>
+                <span className="smallText">Page not found!</span>
+            </ContentWrapper>
+        </div>
+    );
+};
 
-export default Error404
+export default PageNotFound;
--- src/pages/searchResult/SearchResult.jsx
+++ src/pages/searchResult/SearchResult.jsx
@@ -1,9 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import InfiniteScroll from "react-infinite-scroll-component";
-
-import "./SearchResults.scss";
-
+import "./SearchResult.scss";
 import { fetchDataFromApi } from "../../utils/Api";
 import ContentWrapper from "../../components/contentWrapper/ContentWrapper";
 import MovieCard from "../../components/movieCard/MovieCard";
